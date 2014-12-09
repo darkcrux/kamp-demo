382039f2d499fb60f4469f2af06a8fd318db4565
Dexter John Genterone <darkcrux@darkbook-pro.local>
2014-12-09 21:59:49 +0800

    just a demo

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM debian
+MAINTAINER dexter.genterone@gmail.com
+
+RUN apt-get update && apt-get install -y apache2 php5 php-pear php5-mysql
+
+ADD index.php /var/www/demo.php
+
+EXPOSE 80
+
+CMD /usr/sbin/apache2ctl -D FOREGROUND
---
+++ demo.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+  <title>Dem PHP</title>
+</head>
+
+<body>
+
+<h3>
+<?php 
+
+echo "Webapp Hostname: " . gethostname() . "<br />";
+
+$servername =  getenv('MYSQL_SERVICE_HOST') . ":" . getenv('MYSQL_SERVICE_PORT');
+$username = 'root';
+$password = '';
+
+// Create connection
+$conn = mysql_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed to MYSQL failed");
+}
+
+echo "Connected to MYSQL!<br />";
+
+$query = "select @@hostname";
+$result = mysql_query($query);
+
+echo "Query executed!<br />";
+
+$num = mysql_numrows($result);
+while ($i < $num) {
+  $host = mysql_result($result,$i,"@@hostname");
+  $i++;
+  echo "MYSQL: " . $host;
+}
+
+mysql_close($conn);
+?>
+</h3>
+
+</body>
+
+</html>
